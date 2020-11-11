09ecd11193ff02fa55fd0eaef5ae00f04414e7e0
yangsen <yang_sen_ysys@163.com>
2020-11-11 11:09:08 +0800

    init

---
+++ build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs/qn-curl/native/curl_jni_call_back.o.d
@@ -0,0 +1,35 @@
+/Users/senyang/Desktop/QiNiu_Android/curl/build/intermediates/ndkBuild/debug/obj/local/armeabi-v7a/objs/qn-curl/native/curl_jni_call_back.o: \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/native/curl_jni_call_back.cpp \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/native/curl_jni_call_back.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/native/curl_context.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/curl.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/curlver.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/system.h \
+  /Users/senyang/Library/Android/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /Users/senyang/Library/Android/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__config \
+  /Users/senyang/Library/Android/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/limits.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/easy.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/multi.h \
+  /Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/urlapi.h
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/native/curl_jni_call_back.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/native/curl_context.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/curl.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/curlver.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/system.h:
+
+/Users/senyang/Library/Android/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/Users/senyang/Library/Android/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/__config:
+
+/Users/senyang/Library/Android/sdk/ndk/21.3.6528147/sources/cxx-stl/llvm-libc++/include/limits.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/easy.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/multi.h:
+
+/Users/senyang/Desktop/QiNiu_Android/curl/src/main/jni/lib/include/curl/urlapi.h:
